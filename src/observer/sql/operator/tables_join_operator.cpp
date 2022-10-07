d7b03e5fd51dedb0fbff2113c9fddfd106923b27
Yunfan Li <lyf_liyunfan@sina.com>
2022-10-07 15:58:41 +0800

    fix: join_tables, change cartesian_product implement

--- src/observer/sql/operator/tables_join_operator.cpp
+++ src/observer/sql/operator/tables_join_operator.cpp
@@ -4,6 +4,8 @@
 
 #include "tables_join_operator.h"
 #include "table_scan_operator.h"
+#include "sql/stmt/filter_stmt.h"
+#include "storage/common/field.h"
 
 RC TablesJoinOperator::open()
 {
@@ -17,32 +19,45 @@ RC TablesJoinOperator::open()
         auto tuple = static_cast<RowTuple*>(scan_oper->current_tuple());
         table_records.push_back(new Record(tuple->record().rid(), tuple->record().data()));
       }
+
+      for (auto &field : *scan_oper->table()->table_meta().field_metas())
+      {
+        speces_.push_back(new TupleCellSpec(new FieldExpr(scan_oper->table(), &field)));
+      }
+
       tuple_.add_schema(scan_oper->table(), scan_oper->table()->table_meta().field_metas());
       tuple_.add_sum(tuple_length_);
+      field_length.push_back(tuple_length_);
       tuple_length_ += scan_oper->table()->table_meta().field_num();
       record_length_ += table_records.size();
       index_mul_.push_back({(int32_t)table_records.size(), total_index_});
       total_index_ *= table_records.size();
       records_.push_back(table_records);
+      dfs_index_record_.push_back(0);
     } else {
       return rc;
     }
   }
+
+  current_records_.clear();
+  cartesian_product_dfs_(0, 0);
   return RC::SUCCESS;
 }
 
 RC TablesJoinOperator::next()
 {
-  if (current_index_ >= total_index_) {
+  if (current_index_ >= product_records_.size()) {
     return RC::RECORD_EOF;
   }
-  current_records_.clear();
-  for (int32_t table_index = 0; table_index < (int32_t) records_.size(); table_index++)
-  {
-    auto current_row = records_[table_index];
-    auto new_record = current_row[get_row_index_(table_index)];
-    current_records_.push_back(new_record);
-  }
+
+//
+//  for (int32_t table_index = 0; table_index < (int32_t) records_.size(); table_index++)
+//  {
+//    auto current_row = records_[table_index];
+//    auto new_record = current_row[get_row_index_(table_index)];
+//    current_records_.push_back(new_record);
+//  }
+  current_records_ = product_records_[current_index_];
   current_index_++;
   return RC::SUCCESS;
 }
@@ -58,10 +73,97 @@ RC TablesJoinOperator::close()
   return rc;
 }
 
+
 Tuple * TablesJoinOperator::current_tuple()
 {
   tuple_.set_records(&current_records_);
   return &tuple_;
+}
+
+RC TablesJoinOperator::cartesian_product_dfs_(int table_index, int record_length)
+{
+  if (!do_predicate(current_records_, record_length))
+    return RC::GENERIC_ERROR;
+  if (table_index == dfs_index_record_.size()) {
+    product_records_.push_back(current_records_);
+    return SUCCESS;
+  }
+  auto current_row = records_[table_index];
+  for (auto rec : records_[table_index]) {
+    current_records_.push_back(rec);
+    cartesian_product_dfs_(table_index + 1, field_length[table_index]);
+    current_records_.pop_back();
+  }
+//  current_records_.push_back(current_row[dfs_index_record_[table_index]]);
+  return SUCCESS;
+}
+
+int TablesJoinOperator::get_field_index(FieldExpr * fieldExpr)
+{
+  const char *table_name = fieldExpr->field().table_name();
+  const char *field_name = fieldExpr->field().field_name();
+  for (size_t i = 0; i < speces_.size(); ++i) {
+    const FieldExpr * field_expr = (const FieldExpr *)speces_[i]->expression();
+    const Field &field = field_expr->field();
+    if ((0 == strcmp(table_name, field.table_name())) and (0 == strcmp(field_name, field.field_name()))) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+bool TablesJoinOperator::do_predicate( std::vector<Record *> &records, int record_length)
+{
+  if (filter_stmt_ == nullptr || filter_stmt_->filter_units().empty()) {
+    return true;
+  }
+
+  for (const FilterUnit *filter_unit : filter_stmt_->filter_units()) {
+    Expression *left_expr = filter_unit->left();
+    Expression *right_expr = filter_unit->right();
+    if (left_expr->type() == ExprType::FIELD && get_field_index((FieldExpr *)left_expr) >= record_length) {
+      continue;
+    }
+    if (right_expr->type() == ExprType::FIELD && get_field_index((FieldExpr *)right_expr) >= record_length) {
+      continue;
+    }
+    tuple_.set_records(&records);
+    CompOp comp = filter_unit->comp();
+    TupleCell left_cell;
+    TupleCell right_cell;
+    left_expr->get_value(tuple_, left_cell);
+    right_expr->get_value(tuple_, right_cell);
+
+    const int compare = left_cell.compare(right_cell);
+    bool filter_result = false;
+    switch (comp) {
+      case EQUAL_TO: {
+        filter_result = (0 == compare);
+      } break;
+      case LESS_EQUAL: {
+        filter_result = (compare <= 0);
+      } break;
+      case NOT_EQUAL: {
+        filter_result = (compare != 0);
+      } break;
+      case LESS_THAN: {
+        filter_result = (compare < 0);
+      } break;
+      case GREAT_EQUAL: {
+        filter_result = (compare >= 0);
+      } break;
+      case GREAT_THAN: {
+        filter_result = (compare > 0);
+      } break;
+      default: {
+        LOG_WARN("invalid compare type: %d", comp);
+      } break;
+    }
+    if (!filter_result) {
+      return false;
+    }
+  }
+  return true;
 }
 
 int32_t TablesJoinOperator::get_row_index_(int32_t table_num) {
--- src/observer/sql/operator/tables_join_operator.h
+++ src/observer/sql/operator/tables_join_operator.h
@@ -7,11 +7,13 @@
 #include "sql/operator/operator.h"
 #include "sql/operator/table_scan_operator.h"
 
+class FilterStmt;
+
 class TablesJoinOperator: public Operator
 {
 public:
-  TablesJoinOperator(std::vector<TableScanOperator*> scan_opers)
-  :scan_opers_(scan_opers)
+  TablesJoinOperator(std::vector<TableScanOperator*> scan_opers, FilterStmt *filter_stmt)
+  :scan_opers_(scan_opers), filter_stmt_(filter_stmt)
   {
     current_index_ = 0;
     record_length_ = 0;
@@ -40,14 +42,23 @@ public:
 
   Tuple * current_tuple() override;
 private:
+  RC cartesian_product_dfs_(int table_index, int record_length);
+  bool do_predicate( std::vector<Record *> &records , int record_length);
+  int get_field_index(FieldExpr * fieldExpr);
+  std::vector<int32_t> dfs_index_record_;
+
   int32_t get_row_index_(int32_t table_num);
   std::vector<TableScanOperator*> scan_opers_;
   int32_t current_index_;
   int32_t total_index_;
   int32_t record_length_;
   int32_t tuple_length_;
+  std::vector<int32_t> field_length;
   std::vector<std::pair<int32_t, int32_t>> index_mul_;
   std::vector<std::vector<Record *>> records_;
+  std::vector<std::vector<Record *>> product_records_;
   std::vector<Record *> current_records_;
-  JoinTuple tuple_;
+  JoinTuple tuple_, temp_tuple_;
+  FilterStmt *filter_stmt_ = nullptr;
+  std::vector<TupleCellSpec *> speces_;
 };
